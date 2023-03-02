515cd1f3b3ab03ec677f9e68a76477c33b041472
feixinfei <2069209197@qq.com>
2023-03-02 14:17:19 +0800

    完成了学院模块

--- src/utils/downLoad.ts
+++ src/utils/downLoad.ts
@@ -1,7 +1,7 @@
 export const downLoad=(url:string)=>{
   let a=document.createElement("a")
   a.style.display="none";
-  a.href="http://estate.eshareedu.cn/exam/upload/question.xlsx"
+  a.href=url
   document.body.appendChild(a)
   a.click();
   document.body.removeChild(a)
